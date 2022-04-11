600afde0bc6e36727ee33f1d266959eb7ca5bb53
raul <zarater.raul@gmail.com>
2022-04-11 16:05:20 -0700

    attempted the stretch goals

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -81,21 +81,20 @@ Write a function named mapTwoToThe that produces the same output as your forLoop
 and your forEachTwoToThe function from challenge 5, but uses map instead of a for loop or forEach.
 ------------------------------------------------------------------------------------------------ */
 
-const mapTwoToThe = (arr) => arr.map(element => {Math.pow(2, element)});
+const mapTwoToThe = (arr) => arr.map(element => Math.pow(2, element));
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7 - Stretch Goal
 
-Write a function named charCode that, given an array of letters as an input, uses map to return a new array where each element is the result of the `charCodeAt` method on the original array element.
+Write a function named charCode that, given an array of letters as an input, uses map to 
+return a new array where each element is the result of the `charCodeAt` method on the original array element.
 
 Read the MDN documentation on String.charCodeAt() if necessary.
 
 For example: charCode(['h','i']) returns [104, 105].
 ------------------------------------------------------------------------------------------------ */
 
-const charCode = (arr) => {
-  // Solution code here...
-};
+const charCode = (arr) => arr.map();
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 8 - Stretch Goal
