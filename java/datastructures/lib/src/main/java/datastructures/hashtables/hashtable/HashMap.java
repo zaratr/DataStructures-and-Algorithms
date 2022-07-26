33243234c705b478dc71cdd7d5baa3a5939c6bb0
Raul Zarate <zarater.raul@gmail.com>
2022-07-25 19:42:27 -0700

    added changes to hashmap

--- java/datastructures/lib/src/main/java/datastructures/hashtables/hashtable/HashMap.java
+++ java/datastructures/lib/src/main/java/datastructures/hashtables/hashtable/HashMap.java
@@ -4,53 +4,70 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+
+/**
+ * Class hashmap uses only a few arguments. some experimentation had been done with hashfunction
+ * Previous implementation was incorrect do to rushing the implementation.
+ * Current implementation fixes the issue with chaining method.
+ * to see the a few different types of hash function implementation
+ * @see #knuthsHashing(Object)
+ * @see #hashing(Object)
+ * @param <K>
+ * @param <V>
+ */
 public class HashMap<K, V> {
 
   ArrayList<LinkedList<HashMapPair<K,V>>> arr;
-  LinkedList<HashMapPair<K,V>>[] arr2;
-  //private int size;
   private int N;
 
-  //public int λ = 0; // loadfactor n/m
+  /**
+   *
+   * {@code public int λ = 0;} // loadfactor n/m
+   * array of linked list
+   * suppose m = number of slots, n = number elements(keys) in hash table
+   *  https://www.cs.miami.edu/home/odelia/teaching/csc317.sp15/syllabus/
+   * hash tables load factor is n/m. if n = m , then load factor is 1
+   * has a probability of 1 / m of collision for key hashes to each slot!
+   */
 
-  //array of linked list
-  //suppose m = number of slots, n = number elements(keys) in hash table
-  //https://www.cs.miami.edu/home/odelia/teaching/csc317.sp15/syllabus/
-  //hash tables load factor is n/m. if n = m , then load factor is 1
-  //has a probability of 1 / m of collision for key hashes to each slot!
-
+  /**
+   * Constructor with argument that sets the size of the HashMap
+   *
+   * @param size
+   * @throws IllegalAccessException
+   */
   public HashMap(int size) throws IllegalAccessException{
     if(size < 1) throw new IllegalAccessException("too small and must be greater than one.");
 
     this.N = size;
     this.arr = new ArrayList<>(N);
-    this.arr2 = new LinkedList[N];
     for(int i = 0; i<N; ++i)
     {
-      arr2[i] = new LinkedList<>();
       arr.add(i, new LinkedList<>());
     }
   }
 public void set(K key, V value)
 {
+  /**
+   * uses hash function to generate to reference which bucket
+   */
   int index = hashing((K) key);
+  boolean found = false;// uses this line to determine if it is new entry or change the old value
   LinkedList<HashMapPair<K,V>> list = arr.get(index);
-//  LinkedList<HashMapPair<K,V>> list2 = arr2[index];
 
-  if(list.size() == 0)
-  {
-    list.add(new HashMapPair<>(key, value));
-    return;
-  }
+  //change the old value if matches
     for(HashMapPair<K,V> pair : list)
     {
       if(pair.getKey().equals(key)){
         pair.setValue(value);
-        return;
+        found = true;
+        break;
       }
     }
 
-  list.add(new HashMapPair<>(key, value));//TODO: check logic
+    //else - add the value in list
+    if(!found)
+      list.add(new HashMapPair<>(key, value));
     return;
 }
 
@@ -158,46 +175,58 @@ https://www.cs.miami.edu/home/odelia/teaching/csc317.sp15/syllabus/Algorithms5cS
 //      int x = Math.abs(key.hashCode()) % N;
     return x;
   }
-  /*
-    knuths vesion of Multiplication method
-    - math algo is as follows: hashing(k) = ((A*k) mod 2**w)) >> (w-r)
-    - where k = key, is some w bits. a is a random integer. 2**w is the right most w bits, r is the  right most second set of bits on its left most set of bits
-    - shifts bits by using and and adding with >>
-    - the result is then a unique value
+
+  /**
+    * Knuths version of Multiplication Method.<br>
+    *   math algo is as follows: hashing(k) = ((A*k) mod 2**w)) >> (w-r)<br>
+    * where k = key, is some w bits. a is a random integer. 2**w is the right most w bits, r is the  right most second set of bits on its left most set of bits <br>
+    * shifts bits by using and and adding with >><br>
+    * the result is then a unique value <br>
+   * <br><br>
+   *
+   * knuths method - Pseudo Code Algorithm<br>
+   * int k = key;//assume key is an int or .hashcode()<br>
+   * long unsignedValue = ((long) k ) - Integer.MIN_VALUE;<br>
+   * long unsignedIntMax = (1l << 32);<br>
+   * knults multiplication method<br>
+   * long unsignedHashValue = ((unsignedValue * 2654435761l) % unsignedIntMax);<br>
+   * convert back to signed integer<br>
+   * return (int) (unsignedHashValue + Integer.MIN_VALUE);<br>
+   * <a href="https://stackoverflow.com/questions/664014/what-integer-hash-function-are-good-that-accepts-an-integer-hash-key">HashFunction reference</a>
+   * <br><br><br>
+   */
 
 
-   knuths method
-   int k = key;//assume key is an int or .hashcode()
-    long unsignedValue = ((long) k ) - Integer.MIN_VALUE;
-    long unsignedIntMax = (1l << 32);
-    //knults multiplication method
-    long unsignedHashValue = ((unsignedValue * 2654435761l) % unsignedIntMax);
-    //convert bak to signed integer
-    return (int) (unsignedHashValue + Integer.MIN_VALUE);
-    https://stackoverflow.com/questions/664014/what-integer-hash-function-are-good-that-accepts-an-integer-hash-key
+   /**
+   * Multiplication Method (Cormen). Choose m to be a power of 2. Let A be some random-looking real number. Knuth suggests M = 0.5*(sqrt(5) - 1). Then do the following: <br>
+   * <br><br>
+   *
+   * Algorithm - <br>
+   * s = k*A <br>
+   * x = fractional part of s <br>
+   * h(k) = floor(m*x) <br>
+  This seems to be the method that the theoreticians like.<br><br>
 
-
-
-    Multiplication Method (Cormen). Choose m to be a power of 2. Let A be some random-looking real number. Knuth suggests M = 0.5*(sqrt(5) - 1). Then do the following:
-
-     s = k*A
-     x = fractional part of s
-     h(k) = floor(m*x)
-  This seems to be the method that the theoreticians like.
-
-  To do this quickly with integer arithmetic, let w be the number of bits in a word (e.g. 32) and suppose m is 2^p. Then compute:
-
-       s = floor(A * 2^w)
-       x = k*s
-       h(k) = x >> (w-p)      // i.e. right shift x by (w-p) bits
-                              // i.e. extract the p most significant
-                              // bits from x
-
-      https://www.cs.hmc.edu/~geoff/classes/hmc.cs070.200101/homework10/hashfuncs.html
-      http://web.archive.org/web/20071223173210/http://www.concentric.net/~Ttwang/tech/inthash.htm
+  * To do this quickly with integer arithmetic, let w be the number of bits in a word (e.g. 32) and suppose m is 2^p. Then compute: <br>
+  * <br> <br>
+  *     s = floor(A * 2^w)<br>
+  *     x = k*s <br>
+  *     h(k) = x >> (w-p)      // i.e. right shift x by (w-p) bits
+  *                            // i.e. extract the p most significant
+  *                            // bits from x
+  *
+  * <a href="https://www.cs.hmc.edu/~geoff/classes/hmc.cs070.200101/homework10/hashfuncs.html">more about ash function</a>
+   * <a href="http://web.archive.org/web/20071223173210/http://www.concentric.net/~Ttwang/tech/inthash.htm">more about hash functions</a>
    */
 
   //TODO: figure out how to do this in java with a mod on N
+
+  /**
+   * this method is really useful by shifting bits using the golden ratio. also using a prime number
+   * thes methods need some more work
+   * @param key
+   * @return the new has that uses the golden ratio which is extremely useful to avioid collision
+   */
   public int knuthsHashing(K key)
   {
 
--- java/datastructures/lib/src/main/java/datastructures/hashtables/hashtable/HashMapPair.java
+++ java/datastructures/lib/src/main/java/datastructures/hashtables/hashtable/HashMapPair.java
@@ -1,5 +1,7 @@
 package datastructures.hashtables.hashtable;
 
+/**
+ * Funway to do this is using simpleentry
 import java.util.AbstractMap;
 
 public class HashMapPair<K, V> extends AbstractMap.SimpleEntry<K,V>{
@@ -8,4 +10,69 @@ public class HashMapPair<K, V> extends AbstractMap.SimpleEntry<K,V>{
     super(key, value);
   }
 
+}
+ */
+
+/**
+ *
+ */
+
+public class HashMapPair<K, V> {
+  private K key;
+  private V value;
+
+  /**
+   * key/value pair constructor that takes two arguments
+   *
+   * @param key  key represents the first context : [x,...]
+   * @param value  value represents the first context [..., x]
+   */
+  public HashMapPair(K key, V value)
+  {
+    this.key = key;
+    this.value = value;
+  }
+
+
+  /**
+   * Copy Constructor that places data without referencing same memory
+   * @param pair  contains toCopy key/value pair
+   */
+  public HashMapPair(HashMapPair<? extends K, ? extends V> pair)
+  {
+    this.key = pair.key;
+    this.value = pair.value;
+  }
+
+  /**
+   *
+   * @return the value corresponding to this entry
+   */
+  public K getKey()
+  {
+    return key;
+  }
+  /**
+   *
+   * @return the value corresponding to this entry
+   */
+  public V getValue()
+  {
+    return value;
+  }
+
+
+  /**
+   * Replaces the value corresponding to this entry with the specified
+   * value.
+   *
+   * @param value new value to be stored in this entry
+   * @return the old value corresponding to the entry
+   */
+  public V setValue(V value)
+  {
+    V prevValue = value;
+    this.value = value;
+    return prevValue;
+  }
 }
