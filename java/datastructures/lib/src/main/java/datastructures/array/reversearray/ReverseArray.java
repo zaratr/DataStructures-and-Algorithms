7e828ae91bc29813e7cfaf9393dcb16455b463e7
zaratr <zarate.rauljr@gmail.com>
2022-10-06 08:08:54 -0700

    added t code reverse array

--- java/datastructures/lib/src/main/java/datastructures/array/reversearray/ReverseArray.java
+++ java/datastructures/lib/src/main/java/datastructures/array/reversearray/ReverseArray.java
@@ -1,11 +1,17 @@
 package datastructures.array.reversearray;
 
+import java.util.Arrays;
+
 public class ReverseArray {
   public static void main(String [] args)
   {
-    new Main().reverseArray(new int[]{1,2,3});
+    int[] out;
+    if(args.length != 0) out = new ReverseArray().convertArgsandReverse(args);
+    else
+    out = new ReverseArray().reverseArray(new int[]{1,2,3});
+    System.out.println(Arrays.toString(out));
   }
-  public void reverseArray(int [] bigA)
+  public int[] reverseArray(int [] bigA)
   {
     int front=0, back = bigA.length -1, temp = 0;
     while(front < back)
@@ -16,6 +22,18 @@ public class ReverseArray {
       --back;
       ++front;
     }
-    return ;
+    return bigA;
+  }
+
+  public int[] convertArgsandReverse(String[] args)
+  {
+    int[] arr = new int[args.length];
+    for (int i = 0; i < args.length; ++i)
+    {
+      arr[i] = Integer.parseInt(args[i]);
+    }
+    reverseArray(arr);
+
+    return arr;
   }
 }
