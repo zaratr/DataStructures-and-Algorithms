7386268cf237a31e25b021ae58a4417408a04353
Raul Zarate <101022036+zaratr@users.noreply.github.com>
2023-02-05 16:29:22 -0800

    created code for binary

---
+++ java/datastructures/lib/src/main/java/datastructures/array/BinarySearch.java
@@ -0,0 +1,15 @@
+package datastructures.array;
+
+public class BinarySearch {
+  public int binarySearch(int[] arr, int target){
+    int start = 0,end =arr.length,mid = 0;
+
+    for(int i = end - start; i > 0; ++i) {
+      mid = (start + end) / 2;
+      if (arr[mid] == target) return mid;
+      if (target > arr[mid]) start = mid + 1;
+      else end = mid;
+    }
+    return -1;
+  }
+}
---
+++ java/datastructures/lib/src/test/java/datastructures/array/BinarySearchTest.java
@@ -0,0 +1,17 @@
+package datastructures.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BinarySearchTest {
+
+  @Test
+  public void testBinarySearch(){
+    BinarySearch sut = new BinarySearch();
+    int[] test = {1,2,3,4,5,6,7,8,9,10,11};
+    int actual = sut.binarySearch(test, 2);
+    int expected = 1;
+    assertEquals(expected, actual);
+  }
+}
