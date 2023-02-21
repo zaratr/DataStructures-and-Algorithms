9bbbe87cbc01e078656f7b7cb89779c06f8f94fc
zaratr <zarate.rauljr@gmail.com>
2023-02-21 11:14:56 -0800

    added comments

--- cplusplus/source_lib/Arrays/BinarySearch/BinarySearch.h
+++ cplusplus/source_lib/Arrays/BinarySearch/BinarySearch.h
@@ -5,9 +5,20 @@
 #ifndef C___BINARYSEARCH_H
 #define C___BINARYSEARCH_H
 
-
+/** Binary Search divides and finds target number. Method 1 iterates through an array and uses three place holders for the index.
+ * middle holds the location of element that equals the target.
+ * <pre>
+ * Time Complexity : O(lg N)
+ * Space Complexity : O(1)
+ * </pre>
+ */
 class BinarySearch {
 public:
+    /**
+     * traverses a sorted array and finds the mid array by traversing half the array on each instance.
+     * @param array of integers, target integer, size of integer.
+     * @return  int - integer index where element is located
+     */
     int binarySearch(int[], int, int);
 };
 
--- java/datastructures/lib/src/main/java/datastructures/array/binarysearch/BinarySearch.java
+++ java/datastructures/lib/src/main/java/datastructures/array/binarysearch/BinarySearch.java
@@ -1,5 +1,14 @@
 package datastructures.array.binarysearch;
 
+/**
+ * Binary Search divides and finds target number. Method 1 iterates through an array and uses three place holders for the index.
+ * middle holds the location of element that equals the target.
+ * <pre>
+ * Method 1
+ * Time Complexity : O(lg N)
+ * Space Complexity : O(1)
+ * </pre>
+ */
 public class BinarySearch {
   public int binarySearch(int[] arr, int target){
     int start = 0,end =arr.length,mid = 0;
